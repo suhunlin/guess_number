9fa868f816fb85076def77873c8c4959704679cd
suhunlin <lin.suhun@msa.hinet.net>
2022-04-22 17:19:21 +0800

    add count to guess

--- GuessNumber.py
+++ GuessNumber.py
@@ -2,22 +2,30 @@ import random
 from random import randint
 random_num = random.randint(1,100)
 print(random_num)
-user_input = int(input("Please input a number to guess(between 1~100):"))
+count = 0
 
 while True:
-    if random_num == user_input:
-        print("Congratulation !!!")
-        break
+    count += 1
+    if count == 1:
+        user_input = int(input("Please input a number to guess(between 1~100):"))
+        print("You already guess %d times!" % count)
     else:
-        temp_num = user_input
-        if random_num > user_input:
-            user_input = int(input("Please input a number to guess(between 100 ~ %d):" %user_input))
-            if user_input <= temp_num :
-                print("The number toot small!!!Try again!!!")
-                user_input = temp_num
+        if random_num == user_input:
+            print("Congratulation !!!")
+            break
         else:
-            user_input = int(input("Please input a number to guess(between %d ~ 1):" %user_input))
-            if user_input >= temp_num:
-                print("The number toot bigger!!!Try again!!!")
-                user_input = temp_num
+            temp_num = user_input
+            if random_num > user_input:
+                user_input = int(input("Please input a number to guess(between 100 ~ %d):" % user_input))
+                if user_input <= temp_num:
+                    print("The number toot small!!!Try again!!!")
+                    user_input = temp_num
+            else:
+                user_input = int(input("Please input a number to guess(between %d ~ 1):" % user_input))
+                if user_input >= temp_num:
+                    print("The number toot bigger!!!Try again!!!")
+                    user_input = temp_num
+        print("You already guess %d times!" % count)
+
+
 
